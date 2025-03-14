3129f9e9e1915d98aa51052e6fb65d5450e5153a
Renato Marques <renatomarques@tecnico.ulisboa.pt>
2025-03-14 12:40:12 +0000

    fully erased faulty files and components

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import Index from "./pages/Index";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
 import LandingPage from "./pages/LandingPage";
-import ProjectPage from "./pages/ProjectPage";
-import SprintPage from "./pages/SprintPage";
+import Dashboard from "./pages/Dashboard";
 
 const queryClient = new QueryClient();
 
@@ -37,26 +34,8 @@ const App = () => (
                 </ProtectedRoute>
               }
             />
+
             
-            {/* Project page */}
-            <Route
-              path="/project/:projectId"
-              element={
-                <ProtectedRoute>
-                  <ProjectPage />
-                </ProtectedRoute>
-              }
-            />
-            
-            {/* Sprint page with Kanban board */}
-            <Route
-              path="/sprint/:sprintId"
-              element={
-                <ProtectedRoute>
-                  <SprintPage />
-                </ProtectedRoute>
-              }
-            />
             
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
